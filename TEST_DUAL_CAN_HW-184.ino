156a0e41dd09a9e0229f2ca52d17882bbfed9720
bionicbone <github@bionicbone.plus.com>
2022-02-12 20:18:53 +0000

    Dual Can Simplified into a single read call

--- TEST_DUAL_CAN_HW-184.ino
+++ TEST_DUAL_CAN_HW-184.ino
@@ -13,12 +13,11 @@
 long unsigned int rxId;
 unsigned char len = 0;
 unsigned char rxBuf[8];
-char msgString[128];                        // Array to store serial string
-unsigned long timer0 = micros();
-unsigned long timer1 = micros();
+char msgString[128];                            // Array to store serial string
+unsigned long upTimer = millis();
 
 #define CAN0_INT 16                             // Set INT to pin 16
-MCP_CAN CAN0(5);                               // Set CS to pin 10
+MCP_CAN CAN0(22);                               // Set CS to pin 10
 
 // Add 2nd Can Module
 #define CAN1_INT 3                              // Set INT to pin 2
@@ -27,102 +26,75 @@ MCP_CAN CAN1(17);                               // Set CS to pin 10
 
 void setup()
 {
+  // Serial needs to be at least this speed otherwise line will be dropped
   Serial.begin(250000);
-
-  // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+  Serial.println("Dual Can Bus Logger, 500kb/s & 125kb/s...");
+  
+  // Initialize MCP2515 running at 8MHz with a baudrate of 500kb/s and the masks and filters disabled.
   if (CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK)
     Serial.println("CAN0: MCP2515 Initialized Successfully!");
   else
     Serial.println("CAN0: Error Initializing MCP2515...");
-
-  CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
-
-  pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
+    CAN0.setMode(MCP_NORMAL);                              // Set operation mode to normal so the MCP2515 sends acks to received data.
+      pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
 
   // Add 2nd Can Module
   
-  // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
-  if (CAN1.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK)
+  // Initialize MCP2515 running at 8MHz with a baudrate of 125kb/s and the masks and filters disabled.
+  if (CAN1.begin(MCP_ANY, CAN_125KBPS, MCP_8MHZ) == CAN_OK)
     Serial.println("CAN1: MCP2515 Initialized Successfully!");
   else
     Serial.println("CAN1: Error Initializing MCP2515...");
-
-  CAN1.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
-
-  pinMode(CAN1_INT, INPUT);                            // Configuring pin for /INT input
+  CAN1.setMode(MCP_NORMAL);                               // Set operation mode to normal so the MCP2515 sends acks to received data.
+  pinMode(CAN1_INT, INPUT);                               // Configuring pin for /INT input
   
   //SPI.setClockDivider(SPI_CLOCK_DIV4);         // Set SPI to run at 4MHz (8MHz / 2 = 4 MHz)
-                                                  //SPI_CLOCK_DIV2 would be used for 8MHz (16MHz / 2 = 8 MHz)
-
-  
-  Serial.println("MCP2515 Library Receive Example...");
+                                                 //SPI_CLOCK_DIV2 would be used for 8MHz (16MHz / 2 = 8 MHz)
 }
 
 void loop()
 {
   if (!digitalRead(CAN0_INT))                         // If CAN0_INT pin is low, read receive buffer
   {
-    Serial.println(micros() - timer0);
-
-    CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
-
-    if ((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
-      sprintf(msgString, "CAN0: Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
-    else
-      sprintf(msgString, "CAN0: Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
-
-    Serial.print(msgString);
-
-    if ((rxId & 0x40000000) == 0x40000000) {    // Determine if message is a remote request frame.
-      sprintf(msgString, " REMOTE REQUEST FRAME");
-      Serial.print(msgString);
-    }
-    else {
-      for (byte i = 0; i < len; i++) {
-        sprintf(msgString, " 0x%.2X", rxBuf[i]);
-        Serial.print(msgString);
-      }
-    }
-
-    Serial.println();
-
-    timer0 = micros();
+    readCan(CAN0, 0);
   }
-
 
   // Add 2nd Can Module
   if (!digitalRead(CAN1_INT))                         // If CAN0_INT pin is low, read receive buffer
   {
-    Serial.println(micros() - timer1);
-
-    CAN1.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
-
-    if ((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
-      sprintf(msgString, "CAN1: Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
-    else
-      sprintf(msgString, "CAN1: Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
-
-    Serial.print(msgString);
-
-    if ((rxId & 0x40000000) == 0x40000000) {    // Determine if message is a remote request frame.
-      sprintf(msgString, " REMOTE REQUEST FRAME");
-      Serial.print(msgString);
-    }
-    else {
-      for (byte i = 0; i < len; i++) {
-        sprintf(msgString, " 0x%.2X", rxBuf[i]);
-        Serial.print(msgString);
-      }
-    }
-
- 
-    Serial.println();
-
-    timer1 = micros();
+    readCan(CAN1, 1);
   }
-
 }
 
-/*********************************************************************************************************
-  END FILE
-*********************************************************************************************************/
+
+void readCan(MCP_CAN myCan, uint8_t MCP2515number) {
+
+  //Serial.println(micros() - timer1);
+
+  myCan.readMsgBuf(&rxId, &len, rxBuf);      // Read data: rxId = Frame ID, len = data length, buf = data byte(s)
+
+  if ((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
+    sprintf(msgString, "CAN%1d: Extended ID: 0x%.8lX  DLC: %1d  Data:", MCP2515number, (rxId & 0x1FFFFFFF), len);
+  else
+    //sprintf(msgString, "CAN%1d: Standard ID: 0x%.3lX       DLC: %1d  Data:", MCP2515number, rxId, len);
+    sprintf(msgString, "%.8d  %1d  %.3lX  %1d  ",millis()-upTimer , MCP2515number, rxId, len);
+
+  Serial.print(msgString);
+
+  if ((rxId & 0x40000000) == 0x40000000) {    // Determine if message is a remote request frame.
+    sprintf(msgString, " REMOTE REQUEST FRAME");
+    Serial.print(msgString);
+  }
+  else {
+    for (byte i = 0; i < len; i++) {
+      sprintf(msgString, " %.2X", rxBuf[i]);
+      Serial.print(msgString);
+    }
+  }
+
+
+  Serial.println();
+
+  //timer1 = micros();
+}
+
